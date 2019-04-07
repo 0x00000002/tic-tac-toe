dd4c7fadbf32fa4bbb2d9ca05ba8ef11a534bc57
tikonoff <tikonoff@gmail.com>
2019-04-08 01:29:20 +1200

    game logic and tests are added

---
+++ src/components/tic-tac-toe.RITEway.js
@@ -0,0 +1,132 @@
+import { checkMove, move } from './tic-tac-toe'
+import { describe, Try } from 'riteway'
+
+const x = 'x'
+const o = 'o'
+const winnerX = [x, null, null, null, x, null, null, null, x]
+const winnerO = [ null, o, null, null, o, null, null, o, null]
+const noWinner = [ null, null, null, null, o, null, null, o, null]
+
+describe('checkMove()', async assert => {
+  assert({
+    given: 'no state',
+    should: 'return false',
+    actual: checkMove(),
+    expected: false
+  })
+
+  assert({
+    given: 'wrong state',
+    should: 'return false',
+    actual: checkMove([0,1,2], x),
+    expected: false
+  })
+
+  assert({
+    given: 'wrong state type',
+    should: 'throw TypeError',
+    actual: Try(checkMove, null, x),
+    expected: new TypeError()
+  })
+
+  assert({
+    given: 'wrong player type',
+    should: 'return false',
+    actual: Try(checkMove, [], {}),
+    expected: false
+  })
+
+  assert({
+    given: 'X winner state and no player',
+    should: 'return true',
+    actual: checkMove(winnerX),
+    expected: true
+  })
+
+  assert({
+    given: 'X winner state and player X',
+    should: 'return true',
+    actual: checkMove(winnerX, x),
+    expected: true
+  })
+
+  assert({
+    given: 'X winner state and player O',
+    should: 'return true',
+    actual: checkMove(winnerX, o),
+    expected: false
+  })
+
+  assert({
+    given: 'O winner state and no player',
+    should: 'return false',
+    actual: checkMove(winnerO),
+    expected: false
+  })
+
+  assert({
+    given: 'O winner state and player O',
+    should: 'return false',
+    actual: checkMove(winnerO, o),
+    expected: true
+  })
+
+  assert({
+    given: 'O winner state and player X',
+    should: 'return false',
+    actual: checkMove(winnerO, x),
+    expected: false
+  })
+
+  assert({
+    given: 'NO winner state and player X',
+    should: 'return false',
+    actual: checkMove(noWinner, x),
+    expected: false
+  })
+
+  assert({
+    given: 'NO winner state and player O',
+    should: 'return false',
+    actual: checkMove(noWinner, o),
+    expected: false
+  })
+})
+
+
+describe('move()', async assert => {
+  assert({
+    given: 'new move',
+    should: 'return new state',
+    actual: move(noWinner, 'x', 0),
+    expected: [ 'x', null, null, null, 'o', null, null, 'o', null ]
+  })
+
+  assert({
+    given: 'no state',
+    should: 'create new state',
+    actual: move([], 'x', 0),
+    expected: [ 'x' ]
+  })
+
+  assert({
+    given: 'no player',
+    should: 'return new state from player X',
+    actual: move(noWinner),
+    expected: [ 'x', null, null, null, 'o', null, null, 'o', null ]
+  })
+
+  assert({
+    given: 'no index',
+    should: 'return new state from player X',
+    actual: move(noWinner, 'o'),
+    expected: [ 'o', null, null, null, 'o', null, null, 'o', null ]
+  })
+
+  assert({
+    given: 'move to existing index',
+    should: 'return old state',
+    actual: move(noWinner, 'x', 4),
+    expected: [ null, null, null, null, 'o', null, null, 'o', null ]
+  })
+})
---
+++ src/components/tic-tac-toe.js
@@ -0,0 +1,28 @@
+import R from 'ramda'
+import produce from "immer"
+
+const winPatterns = [
+  // rows
+  [true, true, true, false, false, false, false, false, false],
+  [false, false, false, true, true, true, false, false, false],
+  [false, false, false, false, false, false, true, true, true],
+  // cols
+  [true, false, false, true, false, false, true, false, false],
+  [false, true, false, false, true, false, false, true, false],
+  [false, false, true, false, false, true, false, false, true],
+  // diagonals
+  [true, false, false, false, true, false, false, false, true],
+  [false, false, true, false, true, false, true, false, false],
+]
+
+const checkMove = (state = [], player = 'x') => {
+  // convert current player's state to an array similar to the `winPatterns`
+  const booleanState = state.map(i => i === player)
+  // use Array.prototype.some() to avoid checking all patterns
+  return winPatterns.some(pattern => R.equals(pattern, booleanState))
+}
+
+const move = (state = [], player = 'x', idx = 0) =>
+  produce(state, draftState => { draftState[idx] = state[idx] || player })
+
+export { checkMove, move }
