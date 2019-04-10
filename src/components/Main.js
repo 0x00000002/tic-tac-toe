e0d133676d1864616ab60515cf6f306c83bc01dc
tikonoff <tikonoff@gmail.com>
2019-04-10 21:41:19 +1200

    refactoring(components): minor changes
    Fixes #5

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,10 +3,10 @@ import Loading from './Loading'
 import TicTacToe from './TicTacToe'
 
 const Main = ({ game, move }) =>
-  game && game.state && move ? (
-    <section id={'game'}>
-      <TicTacToe state={game.state} move={move} />
-    </section>
-  ) : <Loading />
+  game && game.state && move
+    ? ( <section id={'game'}>
+          <TicTacToe state={game.state} move={move} />
+        </section> )
+    : <Loading />
 
 export default Main
