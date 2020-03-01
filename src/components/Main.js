217fecd690f7772861707e9c1523d3224ae96f08
tikonoff <tikonoff@gmail.com>
2020-03-01 15:19:35 +0300

    small changes

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,18 +4,26 @@ import TicTacToe from './TicTacToe'
 import Result from './Result'
 import * as types from '../state/sagas/types'
 
-const Main = (props) => {
-  const { game, dispatch } = props.control
-  const data = game && game.data
-  return data
-    ? (
-      <section id={'game'} className={data.winner}>
-        <TicTacToe data={data} dispatch={dispatch} />
-        <button onClick={e => dispatch({ type: types.NEW_GAME_REQUEST })} className={'restart'}>Restart</button>
-        <Result winner={data.winner} />
-      </section>
-    )
-    : <Loading />
+const Main = ({ control }) => {
+  const {
+    game: { data },
+    dispatch
+  } = control
+
+  return data ? (
+    <section id='game' className={data.winner}>
+      <TicTacToe data={data} dispatch={dispatch} />
+      <button
+        onClick={e => dispatch({ type: types.NEW_GAME_REQUEST })}
+        className='restart'
+      >
+        Restart
+      </button>
+      <Result winner={data.winner} />
+    </section>
+  ) : (
+    <Loading />
+  )
 }
 
 export default Main
